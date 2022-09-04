dc03bdc8d24a7bdb3b82f2c596582d0617a53ad4
alekcdn <alex3l@mail.ru>
2022-09-04 18:30:44 +0300

    edit program.cs

--- controlwork/Program.cs
+++ controlwork/Program.cs
@@ -3,7 +3,7 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
-string[] arr = new string[5] {"22", "12345", ":0)", "hello", "world"};
+string[] arr = new string[5] { "22", "12345", ":0)", "hello", "world" };
 string[] newarr = new string[arr.Length];
 CrateModArray(arr, newarr);
 Console.WriteLine("Изначальный массив :");
@@ -16,19 +16,19 @@ void CrateModArray(string[] arr, string[] arr1)
     int count = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-    if(arr[i].Length <= 3)
+        if (arr[i].Length <= 3)
         {
-        arr1[count] = arr[i];
-        count++;
+            arr1[count] = arr[i];
+            count++;
         }
     }
 }
 
 void PrintArray(string[] array)
-{   
+{
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
-}    
+}
